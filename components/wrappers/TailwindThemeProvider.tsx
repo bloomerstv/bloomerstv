0f1dc1481ce841f4bf6f48f0c5a13159c2902899
Rathod Deven <devenrathodrd@gmail.com>
2023-12-06 12:35:53 +0530

    useEffect for metaThemeCheck

--- components/wrappers/TailwindThemeProvider.tsx
+++ components/wrappers/TailwindThemeProvider.tsx
@@ -30,7 +30,24 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
       window.localStorage.setItem('data-theme', 'light')
       setTheme('light')
     }
+  }
 
+  useEffect(() => {
+    const theme = window.localStorage.getItem('data-theme')
+    if (theme) {
+      document.body.classList.add(theme)
+      document.documentElement.setAttribute('data-theme', theme)
+      // @ts-ignore
+      setTheme(theme)
+    } else {
+      document.body.classList.add('light')
+      document.documentElement.setAttribute('data-theme', 'light')
+      window.localStorage.setItem('data-theme', 'light')
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
     const metaThemeColor = document.querySelector('meta[name=theme-color]')
     if (metaThemeColor) {
       // @ts-ignore
@@ -39,37 +56,8 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
         theme === 'dark' ? '#1e1e1e' : '#ffffff'
       )
     }
-  }
+  }, [theme])
 
-  useEffect(() => {
-    const theme = window.localStorage.getItem('data-theme')
-    const metaThemeColor = document.querySelector('meta[name=theme-color]')
-
-    if (theme) {
-      document.body.classList.add(theme)
-      document.documentElement.setAttribute('data-theme', theme)
-      // @ts-ignore
-      setTheme(theme)
-
-      if (metaThemeColor) {
-        // @ts-ignore
-        metaThemeColor.setAttribute(
-          'content',
-          theme === 'dark' ? '#1e1e1e' : '#ffffff'
-        )
-      }
-    } else {
-      document.body.classList.add('light')
-      document.documentElement.setAttribute('data-theme', 'light')
-      window.localStorage.setItem('data-theme', 'light')
-      setTheme('light')
-
-      if (metaThemeColor) {
-        // @ts-ignore
-        metaThemeColor.setAttribute('content', '#ffffff')
-      }
-    }
-  }, [])
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
