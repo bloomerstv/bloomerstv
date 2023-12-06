bd08ff820199e51c4bd4ec08aecc70d4718539b7
Rathod Deven <devenrathodrd@gmail.com>
2023-12-06 12:47:37 +0530

    og

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,23 @@ declare global {
 export const metadata: Metadata = {
   manifest: '/manifest.json', // we are accessing our manifest file here
   title: 'BloomersTV',
-  description: 'Game Streaming platform for Bloomers of Lens.'
+  description: 'Game Streaming platform for Bloomers of Lens.',
+  openGraph: {
+    title: 'BloomersTV',
+    description: 'Game Streaming platform for Bloomers of Lens.',
+    type: 'profile',
+    siteName: 'BloomersTV',
+    url: 'https://bloomers.tv',
+    images: [
+      // logo image
+      {
+        url: 'https://bloomers.tv/logo.png',
+        width: 512,
+        height: 512,
+        alt: 'BloomersTV Logo'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
