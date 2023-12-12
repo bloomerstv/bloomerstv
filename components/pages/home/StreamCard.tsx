87fba9a67ba7d4c792c3230574ff3199a2904549
Rathod Deven <devenrathodrd@gmail.com>
2023-12-12 16:32:42 +0530

    live viewerscount in thumbanil of live

--- components/pages/home/StreamCard.tsx
+++ components/pages/home/StreamCard.tsx
@@ -5,6 +5,7 @@ import formatHandle from '../../../utils/lib/formatHandle'
 import Link from 'next/link'
 import LiveDiv from '../../ui/LiveDiv'
 import LoadingImage from '../../ui/LoadingImage'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity'
 
 const StreamCard = ({ streamer }: { streamer: StreamerWithProfile }) => {
   return (
@@ -21,9 +22,16 @@ const StreamCard = ({ streamer }: { streamer: StreamerWithProfile }) => {
           className="w-full h-full transition-transform duration-500 ease-in-out transform hover:scale-110"
           alt="thumbnail"
         />
-        <div className="absolute top-4 left-4">
+        <div className="absolute top-4 left-4 start-row gap-x-2">
           <LiveDiv />
+          <div className="start-row gap-x-1 text-white text-base bg-black bg-opacity-80 px-1.5 rounded-md">
+            <PermIdentityIcon fontSize="inherit" />
+            <div className="text-sm font-semibold">{streamer?.liveCount}</div>
+          </div>
         </div>
+
+        {/* live viewers count */}
+        {/* <div className="absolute bottom-4 left-4 "></div> */}
       </div>
       <div className="sm:px-0 px-4 w-full sm:w-[416px] start-row space-x-4">
         <img
