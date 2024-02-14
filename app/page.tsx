1048321a72491ffbb703374d28574d566757fc18
Rathod Deven <devenrathodrd@gmail.com>
2024-02-14 21:49:01 +0530

    removed test video from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import LiveStreamPublicReplays from '../components/pages/home/LiveStreamPublicRe
 import LiveStreamerFeed from '../components/pages/home/LiveStreamerFeed'
 // import VideosFeed from '../components/pages/home/VideosFeed'
 import useIsMobile from '../utils/hooks/useIsMobile'
-import Video from '../components/common/Video'
 
 export default function Home() {
   const isMobile = useIsMobile()
@@ -22,12 +21,6 @@ export default function Home() {
       {/* top header */}
       {isMobile && <MobileTopHeader />}
 
-      {/* <Video src="https://dev-bloomerstv-api.onrender.com/api/livestream?sessionId=5084354e-7900-458d-b3bc-ff650ffd3da6&format=.m3u8" /> */}
-
-      {/* <Video src="http://localhost:8000/api/livestream?sessionId=5084354e-7900-458d-b3bc-ff650ffd3da6&format=.m3u8" /> */}
-
-      <Video src="https://redirector-git-dev-diversehq-xyz.vercel.app/livestream?sessionId=5084354e-7900-458d-b3bc-ff650ffd3da6&format=.m3u8" />
-
       <LiveStreamerFeed />
       <div className="mt-4">
         <LiveStreamPublicReplays />
@@ -37,9 +30,6 @@ export default function Home() {
           <ClipsFeed />
         </div>
       )}
-      {/* <div className="mt-4">
-        <VideosFeed />
-      </div> */}
     </div>
   )
 }
