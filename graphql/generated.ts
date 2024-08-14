afb537fd66e237386231a0b8a6e5ae0a777d46e2
Rathod Deven <devenrathodrd@gmail.com>
2024-08-14 09:25:58 +0530

    chore: update deps

--- graphql/generated.ts
+++ graphql/generated.ts
@@ -451,6 +451,10 @@ export type OfflineStreamersQuery = {
   } | null> | null
 }
 
+export type QueryQueryVariables = Exact<{ [key: string]: never }>
+
+export type QueryQuery = { __typename?: 'Query'; ping?: any | null }
+
 export type RemoveNotificationSubscriberFromStreamerMutationVariables = Exact<{
   profileId: Scalars['String']['input']
 }>
@@ -1300,6 +1304,63 @@ export type OfflineStreamersSuspenseQueryHookResult = ReturnType<
 export type OfflineStreamersQueryResult = Apollo.QueryResult<
   OfflineStreamersQuery,
   OfflineStreamersQueryVariables
+>
+export const QueryDocument = gql`
+  query Query {
+    ping
+  }
+`
+
+/**
+ * __useQueryQuery__
+ *
+ * To run a query within a React component, call `useQueryQuery` and pass it any options that fit your needs.
+ * When your component renders, `useQueryQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useQueryQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useQueryQuery(
+  baseOptions?: Apollo.QueryHookOptions<QueryQuery, QueryQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<QueryQuery, QueryQueryVariables>(
+    QueryDocument,
+    options
+  )
+}
+export function useQueryLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<QueryQuery, QueryQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<QueryQuery, QueryQueryVariables>(
+    QueryDocument,
+    options
+  )
+}
+export function useQuerySuspenseQuery(
+  baseOptions?: Apollo.SuspenseQueryHookOptions<QueryQuery, QueryQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useSuspenseQuery<QueryQuery, QueryQueryVariables>(
+    QueryDocument,
+    options
+  )
+}
+export type QueryQueryHookResult = ReturnType<typeof useQueryQuery>
+export type QueryLazyQueryHookResult = ReturnType<typeof useQueryLazyQuery>
+export type QuerySuspenseQueryHookResult = ReturnType<
+  typeof useQuerySuspenseQuery
+>
+export type QueryQueryResult = Apollo.QueryResult<
+  QueryQuery,
+  QueryQueryVariables
 >
 export const RemoveNotificationSubscriberFromStreamerDocument = gql`
   mutation RemoveNotificationSubscriberFromStreamer($profileId: String!) {
